b3af83bb2672c65c55a9f2e2a4933fd6832d8924
jakeng <s38vSVPuh58RGRF>
2022-07-27 16:23:03 +0200

    part 1 done'

--- Assignment.R
+++ Assignment.R
@@ -1,18 +1,21 @@
 ##  Part I: Writing complex functions in R
 ### Task 1: Functions to estimate regression models using the SSVS prior
 
-#Loading the data
+#Loading required libraries
 library(BMS)
-#data(datafls)
+library(ggplot2)
+library(reshape2)
+
+#Loading data
 Y<-matrix(datafls$y)
 X<-as.matrix(datafls[2:42])
 
-bayes <- function(X,Y,nsave=1000,nburn=1000) {
+bayes <- function(X,Y,nsave=1000,nburn=1000, tau0=0.01, tau1=10) {
   #Start with actual estimation and Gibbs preliminaries
   ntot <- nsave+nburn
   #Prior prelims for SSVS
-  tau0 <- 0.01
-  tau1 <- 10
+  #tau0 <- 0.01
+  #tau1 <- 10
   
   s0 <- 0.01
   S0 <- 0.01
@@ -70,4 +73,31 @@ bayes <- function(X,Y,nsave=1000,nburn=1000) {
   return(cbind(PIP.mean,A.mean,SIG.mean))
 }
 
-bayes(X,Y)
+## Testing different values for posterior inclusion probabilities (PIP.mean)
+pip1 <- bayes(X,Y)[,1] #tau0=0.01, tau1=10
+pip2 <- bayes(X,Y,tau0=0.001,tau1=50)[,1]
+pip3 <- bayes(X,Y,tau0=0.00001,tau1=1000)[,1]
+pip4 <- bayes(X,Y,tau0=1*10^-15)[,1]
+pip_df <- as.data.frame(cbind(scale=1:41,pip1,pip2,pip3,pip4))
+pip_df <- melt(pip_df, id.vars='scale',variable.name = 'series')
+pip_plot <- ggplot(pip_df, aes(scale,value)) + geom_line(aes(colour = series))+ylab("PIP mean")+xlab("")+ggtitle("Visualizing PIP mean for different values of tau0 and tau1")
+pip_plot
+
+pip_df <- as.data.frame(cbind(scale=1:41,bayes(X,Y)))
+X_norm <- scale(X)
+Y_norm <- scale(Y)
+pip_df_stand <- as.data.frame(cbind(scale=1:41,bayes(X_norm,Y_norm)))
+pip_plot_diff <- ggplot(pip_df_stand, aes(scale, PIP.mean)) +
+  geom_line() +
+  geom_line(data=pip_df, aes(y=PIP.mean), colour='red')
+pip_plot_diff
+
+matr_norm <- matrix(cbind(mean(X_norm),mean(Y_norm),sd(X_norm),sd(Y_norm)),nrow=2)
+colnames(matr_norm) <- c("Std","Mean")
+rownames(matr_norm) <- c("X","Y")
+matr_norm
+
+matr <- matrix(cbind(mean(X),mean(Y),sd(X),sd(Y)),nrow=2)
+colnames(matr) <- c("Std","Mean")
+rownames(matr) <- c("X","Y")
+matr
